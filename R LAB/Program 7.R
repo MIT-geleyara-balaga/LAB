3c68e882200dd55db21c32d2d73215125da153d5
MIT-geleyara-balaga <mitgeleyarabalaga@gmail.com>
2024-11-17 15:00:24 +0530

    Delete R LAB directory

--- R LAB/Program 7.R
+++
@@ -1,9 +0,0 @@
-matrix_A <- matrix(1:4,nrow = 2)
-matrix_B <- matrix(5:8,nrow = 2)
-
-determinant_A <- det(matrix_A)
-cat("Determinant of Matrix A : ",determinant_A,"\n")
-
-inverse_A <- solve(matrix_A)
-cat("Inverse of matrix A : \n")
-print(inverse_A)
