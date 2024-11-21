81b3ecef35e841c5f889790997dc5564e54f4141
MIT-geleyara-balaga <mitgeleyarabalaga@gmail.com>
2024-11-21 21:10:05 +0530

    Update 07.R

--- R LAB/07.R
+++ R LAB/07.R
@@ -1,9 +1,41 @@
-matrix_A <- matrix(1:4,nrow = 2)
-matrix_B <- matrix(5:8,nrow = 2)
+#create vector and matrix
+v1<-c(1,2,3)
+v2<-c(4,5,6)
+m1<-matrix(c(1,2,3,4,5,6),nrow=3,ncol=2)
+m2<-matrix(7:12,nrow=2,byrow=TRUE)
 
-determinant_A <- det(matrix_A)
-cat("Determinant of Matrix A : ",determinant_A,"\n")
+#basic operations
+#vector addition
+vadd<-v1+v2
+print("vector addition")
+print(vadd)
 
-inverse_A <- solve(matrix_A)
-cat("Inverse of matrix A : \n")
-print(inverse_A)
+#scalar multiplication(matrix)
+scalar<-2
+scalMult<-scalar*(m1)
+print("scalar multiplication")
+print(scalMult)
+
+#transpose.matrix
+transp<-t(m2)
+print("transpose")
+print(transp)
+
+# Calculate the determinant
+determinant_A <- det(m1)
+cat("Determinant of m1 : ",determinant_A,"\n")
+
+# Print the result
+print("result")
+
+#inverse
+result<-solve(m1)
+print("inverse")
+print(result)
+
+#eigen values and eigen vectors
+eigen_result<-eigen(m1)
+print("eigen value")
+print(eigen_result $ value)
+print("eigen vectors")
+print(eigen_result $ result)
